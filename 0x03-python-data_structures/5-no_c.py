365c3a29c0793ffa23721a3cd664901aae1c2fc2
atika <atikamohammad8@gmail.com>
2024-01-08 16:40:00 +0300

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    our_string = []
+    for n in range(len(my_string)):
+        if my_string[n] != 'c' and my_string[n] != 'C':
+            our_string += my_string[n]
+            return (our_string)
