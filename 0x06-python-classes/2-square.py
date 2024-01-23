1fdf8dff4956d8df61f8b13d6e623385a923831a
atika <atikamohammad8@gmail.com>
2024-01-23 16:27:15 +0300

    0x05-python-exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
         """Initialize a new Square.
+
         Args:
             size (int): The size of the new square.
         """
