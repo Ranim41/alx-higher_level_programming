b8be7b41697180140543c6aaecf0335ab86d81a2
atika <atikamohammad8@gmail.com>
2024-01-23 16:57:30 +0300

    0x05-python-exceptions

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-import math
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
-"""magic class
-"""
+import math
 
 
 class MagicClass:
-    """Class that stores the properties
-    of a circumference
-    """
+    """Represent a circle."""
 
     def __init__(self, radius=0):
-        """Initialize the class with a radius"""
+        """Initialize a MagicClass.
+
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """Method that calculates the area of a circumference"""
-        return (self.__radius**2) * math.pi
+        """Return the area of the MagicClass."""
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """Method that calculates the perimeter of a circumference"""
-        return 2 * math.pi * self.__radius
+        """Return The circumference of the MagicClass."""
+        return (2 * math.pi * self.__radius)
