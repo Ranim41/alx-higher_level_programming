7e004c7212a9ebf6bee9c5279eebaacc29df6e1b
atika <atikamohammad8@gmail.com>
2024-01-30 21:02:17 +0300

    0x09-python-everything_is_object

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 101-lazy_matrix_mul.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Defines a matrix multiplication function using NumPy."""
-import numpy as np
+import  numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
