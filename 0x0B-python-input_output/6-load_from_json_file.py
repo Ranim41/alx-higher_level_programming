aceabf2daf2357bef6d001908d69124040b9ad40
atika <atikamohammad8@gmail.com>
2024-02-06 18:46:10 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+toooka
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    toooka
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
