feb40c292145c70a490bdf3ce4c2dd50902800b1
atika <atikamohammad8@gmail.com>
2024-02-06 18:59:59 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+tooka
+"""
+
+
+class student:
+    """
+    class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        tooka
+        """
+        return self.__dict__.copy()
