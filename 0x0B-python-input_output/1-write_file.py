e807fe02f884e7f0f9411ac2ca045a4fffaae0e5
atika <atikamohammad8@gmail.com>
2024-02-06 18:17:35 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+took
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    ranooo
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
