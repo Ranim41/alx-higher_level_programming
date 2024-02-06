db977c19dbdecf19d3fb86f93965621ff1bda6d7
atika <atikamohammad8@gmail.com>
2024-02-06 19:29:09 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+toooka
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append """
+
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
