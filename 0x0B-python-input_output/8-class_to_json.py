36de69cc2709659ee785e0825003b534862f4869
atika <atikamohammad8@gmail.com>
2024-02-06 18:54:17 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+atooka
+"""
+
+
+def class_to_json(obj):
+    """
+    atooka
+    """
+
+    ranim = {}
+    if hasattr(obj, "__dict__"):
+        ranim = obj.__dict__.copy()
+    return ranim
