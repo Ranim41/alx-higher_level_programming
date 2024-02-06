561a11f8920190f9211aec68cae276c3791d98f1
atika <atikamohammad8@gmail.com>
2024-02-06 19:01:50 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,16 +4,12 @@ tooka
 """
 
 
-class student:
+class Student:
     """
     class
     """
 
     def __init__(self, first_name, last_name, age):
-        """
-        toka
-        """
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
